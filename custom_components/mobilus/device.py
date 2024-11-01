5ad0b104e66a7e40ab483c7aa9d2614647c8a841
Zbigniew Pieślak <zpieslak@gmail.com>
2024-11-01 18:32:44 +0100

    Differentiate supported features by device type (#13)
    Add MobilusDevice class that abstracts device
    List all devices supproted by COSMO GTW
    Reject unsupported (non cover) devices

---
+++ custom_components/mobilus/device.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+
+
+class MobilusDevice(IntEnum):
+    SENSO = 1
+    COSMO = 2
+    CMR = 3
+    CGR = 4
+    SWITCH = 5
+    SWITCH_NP = 6
+    COSMO_CZR = 7
+    COSMO_MZR = 8
+    SENSO_Z = 9
